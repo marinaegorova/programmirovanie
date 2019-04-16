4f239dafee320d5a534e19641cac3640a380d3cd
marinaegorova <marinaegorova0808@gmail.com>
2019-04-16 22:15:47 +0900

    second commit

--- zadanie 7/Zadanie7.cpp
+++ zadanie 7/Zadanie7.cpp
@@ -1,54 +1,117 @@
 // Zadanie7.cpp : Defines the entry point for the console application.
 //
-
 #include "stdafx.h"
 #include <string>
-#include "mage.h"
-#include "spell.h"
 #include <iostream>
 using namespace std;
 
 class spell
-
-	{
+{
 public:
-	string name;
+	std::string name;
 	int dhp;
 	int dmp;
-	};
+};
 
 class mage
-	{
+{
 private:
 	int hp;
 	int mp;
-	string name;
+	std::string name;
 
 public:
-	void init( int newhp, int newmp, string newname)
+	void init(int newhp, int newmp, string newname);
+	std::string getname();
+	void say(std::string text);
+	int cast(spell &spl, mage &target);
+	void hit(spell &spl);
+};
+void mage::init(int newhp, int newmp, std::string newname)
+{
+	hp = newhp;
+	mp = newmp;
+	name = newname;
+}
+string mage::getname()
+{
+	return this->name;
+}
+
+void mage::say(std::string text)
+{
+	cout << this->name << " says " << text << endl << endl;
+}
+
+int mage::cast(Spell &spl, mage &target)
+{
+	if (hp > 0)
 	{
-		this->hp=newhp;
-		this->mp=newmp;
+		cout << this->name << " hit with " << spl.name << " rival " << target.getname() << endl << endl;
+		target.hit(spl);
 	}
-	string getname()
+
+	else
 	{
-		return this->name;
+		this->say("I'm defeated");
 	}
-	void say (string text)
+	return 0;
+}
+
+void mage::hit(Spell &spl)
+{
+	this->hp -= spl.dhp;
+	this->mp -= spl.dmp;
+	if (hp <= 0)
 	{
-		cout << this->name << "Say" << text;
+		this->say(" ooohh... I give up");
 	}
-	int cast(spell spl, mage target);
-	void hit(spell spl);
-	};
+}
 
 
+void main()
+{
+	mage Ivan;
+	mage Boris;
+	spell bolt;
+	spell drain;
 
+	bolt.name = "Bolt";
+	bolt.dhp = 44;
+	bolt.dmp = 2;
 
-	int _tmain(int argc, _TCHAR* argv[])
-	{
-	mage vanya, boris;
-	vanya init()
-	
-	}
+	drain.name = "Drain";
+	drain.dhp = 10;
+	drain.dmp = 60;
+
+	Ivan.init(150, 150, "Ivan");
+	Ivan.say("I challenge you");
+
+	Boris.init(150, 150, "Boris");
+	Boris.say("I'm ready");
+
+	Ivan.cast(bolt, Boris);
+	Boris.cast(drain, Ivan);
+
+	Ivan.cast(bolt, Boris);
+	Boris.cast(drain, Ivan);
+
+	Ivan.cast(bolt, Boris);
+	Boris.cast(drain, Ivan);
+
+	Ivan.cast(bolt, Boris);
+	Boris.cast(drain, Ivan);
+
+	Ivan.cast(bolt, Boris);
+	Boris.cast(drain, Ivan);
+
+	Ivan.cast(bolt, Boris);
+	Boris.cast(drain, Ivan);
+
+	Ivan.cast(bolt, Boris);
+	Boris.cast(drain, Ivan);
+
+	Ivan.cast(bolt, Boris);
+	Boris.cast(drain, Ivan);
+}
 
